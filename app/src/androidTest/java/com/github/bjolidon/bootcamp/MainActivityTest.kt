d2438a7c4d3ef393b64e1d69cdb5c5391ea8bc3f
Cocograal <willi1maril@gmail.com>
2023-03-03 19:02:21 +0100

    dummy test updated

--- app/src/androidTest/java/com/github/bjolidon/bootcamp/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/bjolidon/bootcamp/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.github.bjolidon.bootcamp
 
 import android.content.Intent
+import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.*
@@ -20,6 +21,10 @@ class MainActivityTest {
     @get:Rule
     public val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    @Test
+    fun dummyTest() {
+        onView(withId(R.id.mainGoButton)).perform(click())
+    }
 //    @Test
 //    fun testMainActivity() {
 //        Intents.init()
