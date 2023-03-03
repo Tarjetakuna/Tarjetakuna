39d3f1a10fdbfdfb4b048b27b09a9fb7a8e2e58e
KelvinKappeler <kelvin.kappeler@gmail.com>
2023-03-03 18:35:32 +0100

    Added the base for the card model

---
+++ app/src/main/java/com/github/bjolidon/bootcamp/model/MagicColor.kt
@@ -0,0 +1,18 @@
+// ====================
+// MagicColor.kt
+// Tarjetakuna, 2023
+// ====================
+
+package com.github.bjolidon.bootcamp.model
+
+/**
+ * Represents the color of a Magic card.
+ */
+enum class MagicColor {
+    White,
+    Blue,
+    Black,
+    Red,
+    Green,
+    Colorless
+}
---
+++ app/src/main/java/com/github/bjolidon/bootcamp/model/MagicLayout.kt
@@ -0,0 +1,23 @@
+// ====================
+// MagicLayout.kt
+// Tarjetakuna, 2023
+// ====================
+
+package com.github.bjolidon.bootcamp.model
+
+/**
+ * Represents the layout of a Magic card.
+ */
+enum class MagicLayout {
+    Normal,
+    Split,
+    Flip,
+    DoubleFaced,
+    Token,
+    Plane,
+    Scheme,
+    Phenomenon,
+    Leveler,
+    Vanguard,
+    Aftermath
+}
