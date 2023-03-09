aa667d5d0e1b4552e1ee52e6e942743eb9d748b7
KelvinKappeler <kelvin.kappeler@gmail.com>
2023-03-09 09:33:32 +0100

    ViewModel implementation

--- app/src/main/java/com/github/bjolidon/bootcamp/ui/scanner/ScannerViewModel.kt
+++ app/src/main/java/com/github/bjolidon/bootcamp/ui/scanner/ScannerViewModel.kt
@@ -1,13 +1,37 @@
 package com.github.bjolidon.bootcamp.ui.scanner
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
+import android.graphics.Bitmap
+import androidx.activity.result.ActivityResult
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScannerViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Coming soon here: a scanner to scan your cards"
+    private val _textInformation = MutableLiveData<String>()
+    val textInformation: LiveData<String> = _textInformation
+
+    private val _image = MutableLiveData<Bitmap>()
+    val image: LiveData<Bitmap> = _image
+
+    fun setTextInformation(activityResult: Int) {
+        when (activityResult) {
+            RESULT_OK -> {
+                _textInformation.value = "Success"
+            }
+            RESULT_CANCELED -> {
+                _textInformation.value = "The operation has been cancelled. Please try again."
+            }
+            else -> {
+                _textInformation.value = "An error has occurred. Please try again."
+            }
+        }
     }
-    val text: LiveData<String> = _text
+
+    fun setImage(image: Bitmap) {
+        _image.value = image
+    }
+
 }
