bef483e3e114f3a5fe9ff9cf2c130f2cd9bf8e6b
Cocograal <willi1maril@gmail.com>
2023-03-16 12:51:40 +0100

    tests added

--- app/src/main/java/com/github/bjolidon/bootcamp/ui/browser/BrowserFragment.kt
+++ app/src/main/java/com/github/bjolidon/bootcamp/ui/browser/BrowserFragment.kt
@@ -21,7 +21,6 @@ class BrowserFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private val cards: ArrayList<MagicCard> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,15 +32,12 @@ class BrowserFragment : Fragment() {
         _binding = FragmentBrowserBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        // simulate the cards
-        generateCards()
-
         val textView: TextView = binding.textBrowser
         browserViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
 
-        setUpRecyclerView()
+        setUpRecyclerView(browserViewModel)
         return root
     }
 
@@ -50,25 +46,13 @@ class BrowserFragment : Fragment() {
         _binding = null
     }
 
-    private fun setUpRecyclerView() {
-        val recyclerView = binding.listOfCardsRecycleView
+    /**
+     * Set up the recycler view with the cards
+     */
+    private fun setUpRecyclerView(browserViewModel: BrowserViewModel) {
+        val recyclerView = binding.listOfCardsRecyclerView
         recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = DisplayCardsAdapter(cards)
+        recyclerView.adapter = DisplayCardsAdapter(browserViewModel.cards)
     }
 
-    /**
-     * Generate cards in order to simulate the display of the cards
-     */
-    private fun generateCards() {
-        for (i in 0..39) {
-            val name = if (i+1 < 10) { "Magic 190${i+1}" } else { "Magic 19${i+1}" }
-            val card = MagicCard(
-                "Angel of Mercy ${i+1}", "Flying",
-                MagicLayout.Normal, 7, "{5}{W}{W}",
-                MagicSet("MT15", name), 56,
-                "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=149935&type=card"
-            )
-            cards.add(card)
-        }
-    }
 }
--- app/src/main/java/com/github/bjolidon/bootcamp/ui/browser/BrowserViewModel.kt
+++ app/src/main/java/com/github/bjolidon/bootcamp/ui/browser/BrowserViewModel.kt
@@ -3,6 +3,9 @@ package com.github.bjolidon.bootcamp.ui.browser
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.bjolidon.bootcamp.model.MagicCard
+import com.github.bjolidon.bootcamp.model.MagicLayout
+import com.github.bjolidon.bootcamp.model.MagicSet
 
 class BrowserViewModel : ViewModel() {
 
@@ -10,4 +13,25 @@ class BrowserViewModel : ViewModel() {
         value = "Coming soon here: a search bar to filter your cards"
     }
     val text: LiveData<String> = _text
+
+
+    val cards: ArrayList<MagicCard> = generateCards()
+
+    /**
+     * Generate cards in order to simulate the display of the cards
+     */
+    private fun generateCards(): ArrayList<MagicCard> {
+        val cardsArray = ArrayList<MagicCard>()
+        for (i in 0..39) {
+            val name = if (i+1 < 10) { "Magic 190${i+1}" } else { "Magic 19${i+1}" }
+            val card = MagicCard(
+                "Angel of Mercy ${i+1}", "Flying",
+                MagicLayout.Normal, 7, "{5}{W}{W}",
+                MagicSet("MT15", name), 56,
+                "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=149935&type=card"
+            )
+            cardsArray.add(card)
+        }
+        return cardsArray
+    }
 }
