34f11cedb2316ca45b627a906192940953d584ac
KelvinKappeler <kelvin.kappeler@gmail.com>
2023-03-23 17:07:48 +0100

    Beginning of test on click in Browser fragment

--- app/src/androidTest/java/com/github/sdp/tarjetakuna/ui/BrowserFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp/tarjetakuna/ui/BrowserFragmentTest.kt
@@ -4,13 +4,18 @@ import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.sdp.tarjetakuna.R
 import com.github.sdp.tarjetakuna.ui.browser.BrowserFragment
+import com.github.sdp.tarjetakuna.ui.browser.DisplayCardsAdapter
+import com.github.sdp.tarjetakuna.ui.singlecard.SingleCardFragment
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -29,8 +34,8 @@ class BrowserFragmentTest {
 
     @After
     fun after() {
-        Intents.release()
         scenario.close()
+        Intents.release()
     }
 
     @Test
@@ -44,6 +49,12 @@ class BrowserFragmentTest {
             onView(withId(R.id.listOfCardsRecyclerView)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(i))
                 .check(matches(hasDescendant(withText("Ambush Paratrooper ${i+1}"))))
         }
+    }
 
+    @Test
+    fun clickOnItemChangeFragment() {
+        onView(withId(R.id.listOfCardsRecyclerView)).perform(actionOnItemAtPosition<DisplayCardsAdapter.ViewHolder>(0, click()))
+
+        //Intents.intended(hasComponent(SingleCardFragment::class.java.name))
     }
 }
