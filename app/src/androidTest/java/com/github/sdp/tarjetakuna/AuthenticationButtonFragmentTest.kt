5e4168090dd326fd068843e1c29fe53a43367ac9
Cocograal <willi1maril@gmail.com>
2023-03-25 19:57:34 +0100

    refactor done on tests

--- app/src/androidTest/java/com/github/sdp/tarjetakuna/AuthenticationButtonFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp/tarjetakuna/AuthenticationButtonFragmentTest.kt
@@ -16,44 +16,44 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class AuthenticationButtonFragmentTest {
 
-        @get:Rule
-        public val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule
+    public val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
-        @Before
-        fun setUp() {
-            activityRule.scenario.onActivity { activity ->
-                val navController =
-                    Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
-                navController.navigate(R.id.nav_authentication_button)
-            }
+    @Before
+    fun setUp() {
+        activityRule.scenario.onActivity { activity ->
+            val navController =
+                Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
+            navController.navigate(R.id.nav_authentication_button)
         }
+    }
 
-        @Test
-        fun testAuthenticationButtonActivity() {
-            onView(withId(R.id.connectionButton)).perform(click())
-            // Verify that we've navigated back to the right destination
-            activityRule.scenario.onActivity { activity ->
-                val navController =
-                    Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
-                ViewMatchers.assertThat(
-                    navController.currentDestination?.id,
-                    Matchers.equalTo(R.id.nav_authentication)
-                )
-            }
+    @Test
+    fun testAuthenticationButtonActivity() {
+        onView(withId(R.id.connectionButton)).perform(click())
+        // Verify that we've navigated back to the right destination
+        activityRule.scenario.onActivity { activity ->
+            val navController =
+                Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
+            ViewMatchers.assertThat(
+                navController.currentDestination?.id,
+                Matchers.equalTo(R.id.nav_authentication)
+            )
         }
+    }
 
-        @Test
-        fun testAuthenticationButtonActivityBackToHome() {
-            onView(withId(R.id.button_back_home)).perform(click())
+    @Test
+    fun testAuthenticationButtonActivityBackToHome() {
+        onView(withId(R.id.button_back_home)).perform(click())
 
-            // Verify that we've navigated back to the right destination
-            activityRule.scenario.onActivity { activity ->
-                val navController =
-                    Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
-                ViewMatchers.assertThat(
-                    navController.currentDestination?.id,
-                    Matchers.equalTo(R.id.nav_home)
-                )
-            }
+        // Verify that we've navigated back to the right destination
+        activityRule.scenario.onActivity { activity ->
+            val navController =
+                Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
+            ViewMatchers.assertThat(
+                navController.currentDestination?.id,
+                Matchers.equalTo(R.id.nav_home)
+            )
         }
+    }
 }
