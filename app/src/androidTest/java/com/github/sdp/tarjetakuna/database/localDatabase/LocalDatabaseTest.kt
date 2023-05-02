0fae1bba8fea857d53e7fa9d2f02a337cb6b3c7a
Cocograal <willi1maril@gmail.com>
2023-05-02 10:18:58 +0200

    test added

--- app/src/androidTest/java/com/github/sdp/tarjetakuna/database/localDatabase/LocalDatabaseTest.kt
+++ app/src/androidTest/java/com/github/sdp/tarjetakuna/database/localDatabase/LocalDatabaseTest.kt
@@ -1,10 +1,17 @@
 package com.github.sdp.tarjetakuna.database.localDatabase
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.sdp.tarjetakuna.MainActivity
+import com.github.sdp.tarjetakuna.database.DBMagicCard
 import com.github.sdp.tarjetakuna.database.local.AppDatabase
+import com.github.sdp.tarjetakuna.database.local.LocalDatabaseProvider
 import com.github.sdp.tarjetakuna.utils.TemporaryCards.generateCards
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -16,92 +23,90 @@ class LocalDatabaseTest {
 
     private var cards = generateCards().map { it.toDBMagicCard() }
 
-    @Test
-    fun dummyTest() {
+
+    @Before
+    fun setUp() {
+        // database that will disappear after the end of the test
+        LocalDatabaseProvider.debugging = true
+        database =
+            LocalDatabaseProvider.setDatabase(
+                ApplicationProvider.getApplicationContext(), "test", true
+            )!!
+        activityRule = ActivityScenario.launch(MainActivity::class.java)
+
     }
 
-//    @Before
-//    fun setUp() {
-//        // database that will disappear after the end of the test
-//        database =
-//            LocalDatabaseProvider.setDatabase(
-//                ApplicationProvider.getApplicationContext(), "", true
-//            )!!
-//        activityRule = ActivityScenario.launch(MainActivity::class.java)
-//
-//    }
-//
-//    @After
-//    fun tearDown() {
-//        LocalDatabaseProvider.closeDatabase()
-//    }
-//
-//    @Test
-//    fun testInsertCard() {
-//        var databaseCards: List<MagicCardEntity>
-//        runBlocking {
-//            withTimeout(5000) {
-//                database.magicCardDao().insertCard(cards[0])
-//                databaseCards = database.magicCardDao().getAllCards()
-//            }
-//        }
-//        assert(databaseCards.isNotEmpty())
-////        assert(databaseCards[0].name == cards[0].name)
-//        assert(databaseCards.size == 1)
-//    }
-//
-//    @Test
-//    fun testEmptyDatabase() {
-//        var databaseCards: List<MagicCardEntity>
-//        runBlocking {
-//            withTimeout(5000) {
-//                databaseCards = database.magicCardDao().getAllCards()
-//            }
-//        }
-//        assert(databaseCards.isEmpty())
-//    }
-//
-//    @Test
-//    fun testDeleteCard() {
-//        var databaseCards: List<MagicCardEntity>
-//        runBlocking {
-//            withTimeout(5000) {
-//                database.magicCardDao().insertCard(cards[5])
-//                database.magicCardDao().deleteCard(cards[5])
-//                databaseCards = database.magicCardDao().getAllCards()
-//            }
-//        }
-//        assert(databaseCards.isEmpty())
-//    }
-//
-//    @Test
-//    fun testAdd2Cards() {
-//        var databaseCards: List<MagicCardEntity>
-//        runBlocking {
-//            withTimeout(5000) {
-//                database.magicCardDao().insertCard(cards[0])
-//                database.magicCardDao().insertCard(cards[1])
-//                databaseCards = database.magicCardDao().getAllCards()
-//            }
-//        }
-//        assert(databaseCards.isNotEmpty())
-////        assert(databaseCards[0].name == cards[0].name)
-////        assert(databaseCards[1].name == cards[1].name)
-//        assert(databaseCards.size == 2)
-//    }
-//
-//    @Test
-//    fun testDeleteAll() {
-//        var databaseCards: List<MagicCardEntity>
-//        runBlocking {
-//            withTimeout(5000) {
-//                for (i in 0..5) {
-//                    database.magicCardDao().insertCard(cards[i])
-//                }
-//                database.magicCardDao().deleteAllCards()
-//                databaseCards = database.magicCardDao().getAllCards()
-//            }
-//        }
-//        assert(databaseCards.isEmpty())
-//    }
+    @After
+    fun tearDown() {
+        LocalDatabaseProvider.closeDatabase("test")
+    }
+
+    @Test
+    fun testInsertCard() {
+        var databaseCards: List<DBMagicCard>
+        runBlocking {
+            withTimeout(5000) {
+                database.magicCardDao().insertCard(cards[0])
+                databaseCards = database.magicCardDao().getAllCards()
+            }
+        }
+        assert(databaseCards.isNotEmpty())
+        assert(databaseCards[0].card == cards[0].card)
+        assert(databaseCards.size == 1)
+    }
+
+    @Test
+    fun testEmptyDatabase() {
+        var databaseCards: List<DBMagicCard>
+        runBlocking {
+            withTimeout(5000) {
+                databaseCards = database.magicCardDao().getAllCards()
+            }
+        }
+        assert(databaseCards.isEmpty())
+    }
+
+    @Test
+    fun testDeleteCard() {
+        var databaseCards: List<DBMagicCard>
+        runBlocking {
+            withTimeout(5000) {
+                database.magicCardDao().insertCard(cards[5])
+                database.magicCardDao().deleteCard(cards[5])
+                databaseCards = database.magicCardDao().getAllCards()
+            }
+        }
+        assert(databaseCards.isEmpty())
+    }
+
+    @Test
+    fun testAdd2Cards() {
+        var databaseCards: List<DBMagicCard>
+        runBlocking {
+            withTimeout(5000) {
+                database.magicCardDao().insertCard(cards[0])
+                database.magicCardDao().insertCard(cards[1])
+                databaseCards = database.magicCardDao().getAllCards()
+            }
+        }
+        assert(databaseCards.isNotEmpty())
+        assert(databaseCards[0].card == cards[0].card)
+        assert(databaseCards[1].card == cards[1].card)
+        assert(databaseCards.size == 2)
+    }
+
+    @Test
+    fun testDeleteAll() {
+        var databaseCards: List<DBMagicCard>
+        runBlocking {
+            withTimeout(5000) {
+                for (i in 0..5) {
+                    database.magicCardDao().insertCard(cards[i])
+                }
+                database.magicCardDao().deleteAllCards()
+                databaseCards = database.magicCardDao().getAllCards()
+            }
+        }
+        assert(databaseCards.isEmpty())
+    }
 }
