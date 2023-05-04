f0277b6de078e7a73091c68549e62d51f35b27ce
Anne-Marie Rusu <annierusu@gmail.com>
2023-05-04 18:34:08 +0200

    mock auth for auth button

--- app/src/androidTest/java/com/github/sdp/tarjetakuna/AuthenticationButtonFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp/tarjetakuna/AuthenticationButtonFragmentTest.kt
@@ -6,11 +6,15 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.sdp.tarjetakuna.ui.authentication.Authenticator
+import com.github.sdp.tarjetakuna.ui.authentication.SignIn
+import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 class AuthenticationButtonFragmentTest {
@@ -20,6 +24,10 @@ class AuthenticationButtonFragmentTest {
 
     @Before
     fun setUp() {
+        val mockedAuth = Mockito.mock(Authenticator::class.java)
+        Mockito.`when`(mockedAuth.isUserLoggedIn()).thenReturn(true)
+        SignIn.setSignIn(mockedAuth)
+
         activityRule.scenario.onActivity { activity ->
             val navController =
                 Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
@@ -28,36 +36,34 @@ class AuthenticationButtonFragmentTest {
     }
 
     // TODO fix this test because it is not working on cirrus CI for some reason
-//    @Test
-//    fun testAuthenticationButtonActivity() {
-//        onView(withId(R.id.connectionButton)).perform(click())
-//        // Verify that we've navigated back to the right destination
-//        activityRule.scenario.onActivity { activity ->
-//            val navController =
-//                Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
-//
-//            var isRightWindow = false
-//            var index = 0
-//            // Check every one second if it works, maybe it is a race condition
-//            while (!isRightWindow) {
-//                if (navController.currentDestination?.id == R.id.nav_authentication) {
-//                    isRightWindow = true
-//                } else {
-//                    Thread.sleep(1000)
-//                    index += 1
-//                }
-//
-//                if (index == 5) {
-//                    Assert.fail("The fragment is not the right one")
-//                }
-//            }
-//        }
-//    }
+    @Test
+    fun testAuthenticationButtonActivity() {
+        onView(withId(R.id.authenticationButton_sign_in_button)).perform(click())
+        // Verify that we've navigated back to the right destination
+        activityRule.scenario.onActivity { activity ->
+            val navController =
+                Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
+
+            var isRightWindow = false
+            var index = 0
+            // Check every one second if it works, maybe it is a race condition
+            while (!isRightWindow) {
+                if (navController.currentDestination?.id == R.id.nav_authentication) {
+                    isRightWindow = true
+                } else {
+                    Thread.sleep(1000)
+                    index += 1
+                }
+                if (index == 5) {
+                    Assert.fail("The fragment is not the right one")
+                }
+            }
+        }
+    }
 
     @Test
     fun testAuthenticationButtonActivityBackToHome() {
         onView(withId(R.id.authenticationButton_home_button)).perform(click())
-
         // Verify that we've navigated back to the right destination
         activityRule.scenario.onActivity { activity ->
             val navController =
@@ -65,4 +71,5 @@ class AuthenticationButtonFragmentTest {
             assertEquals(navController.currentDestination?.id, R.id.nav_home)
         }
     }
+
 }
