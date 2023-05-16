252ed68475ccf630c045bfe8993e36629e59f0ac
KelvinKappeler <kelvin.kappeler@gmail.com>
2023-05-16 12:49:48 +0200

    Add tabs

--- app/src/main/java/com/github/sdp/tarjetakuna/ui/singleuser/CardListFragment.kt
+++ app/src/main/java/com/github/sdp/tarjetakuna/ui/singleuser/CardListFragment.kt
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.sdp.tarjetakuna.R
+import com.github.sdp.tarjetakuna.database.CardPossession
+import com.github.sdp.tarjetakuna.database.DBMagicCard
 import com.github.sdp.tarjetakuna.ui.browser.DisplayCardsAdapter
+import com.google.gson.Gson
 
 /**
  * A fragment representing a list of Items.
@@ -16,12 +19,15 @@ import com.github.sdp.tarjetakuna.ui.browser.DisplayCardsAdapter
 class CardListFragment : Fragment() {
 
     private var isOwned = true
+    private var cards: List<DBMagicCard> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         arguments?.let {
             isOwned = it.getBoolean(ARG_OWNED_CARDS)
+            cards =
+                Gson().fromJson(it.getString(ARG_DB_CARDS), Array<DBMagicCard>::class.java).toList()
         }
     }
 
@@ -31,11 +37,18 @@ class CardListFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_card_list, container, false)
 
+        val cardsFiltered =
+            cards.filter {
+                if (isOwned)
+                    it.possession == CardPossession.OWNED
+                else it.possession == CardPossession.WANTED
+            }.map { it.toMagicCard() }.toCollection(ArrayList())
+
         // Set the adapter
         if (view is RecyclerView) {
             with(view) {
                 layoutManager = LinearLayoutManager(context)
-                adapter = DisplayCardsAdapter(cards)
+                adapter = DisplayCardsAdapter(cardsFiltered)
             }
         }
         return view
@@ -43,12 +56,14 @@ class CardListFragment : Fragment() {
 
     companion object {
         const val ARG_OWNED_CARDS = "isOwned"
+        const val ARG_DB_CARDS = "dbCards"
 
         @JvmStatic
-        fun newInstance(isOwned: Boolean) =
+        fun newInstance(isOwned: Boolean, dbCards: ArrayList<DBMagicCard>) =
             CardListFragment().apply {
                 arguments = Bundle().apply {
                     putBoolean(ARG_OWNED_CARDS, isOwned)
+                    putString(ARG_DB_CARDS, Gson().toJson(dbCards))
                 }
             }
     }
--- app/src/main/java/com/github/sdp/tarjetakuna/ui/singleuser/SingleUserFragment.kt
+++ app/src/main/java/com/github/sdp/tarjetakuna/ui/singleuser/SingleUserFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.github.sdp.tarjetakuna.R
 import com.github.sdp.tarjetakuna.database.CardPossession
+import com.github.sdp.tarjetakuna.database.DBMagicCard
 import com.github.sdp.tarjetakuna.databinding.FragmentSingleUserBinding
 import com.github.sdp.tarjetakuna.model.User
 import com.google.android.material.tabs.TabLayoutMediator
@@ -19,6 +22,8 @@ class SingleUserFragment : Fragment() {
 
     private var _binding: FragmentSingleUserBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var user: User
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,7 +42,7 @@ class SingleUserFragment : Fragment() {
      */
     private fun loadUserFromJson() {
         try {
-            val user = Gson().fromJson(arguments?.getString(ARG_USER), User::class.java)
+            user = Gson().fromJson(arguments?.getString(ARG_USER), User::class.java)
             binding.singleUserUserNameText.text = user.username
             binding.singleUserUserEmailText.text = user.email
             binding.singleUserUserNumberOwnedCards.text =
@@ -50,6 +55,7 @@ class SingleUserFragment : Fragment() {
                     R.string.single_user_wanted_cards_number,
                     user.cards.filter { it.possession == CardPossession.WANTED }.size
                 )
+            setUsersTabs()
 
         } catch (e: Exception) {
             binding.singleUserUserNameText.text =
@@ -64,7 +70,7 @@ class SingleUserFragment : Fragment() {
      * Sets the tabs to display the users that have the card in their collection or wanted cards.
      */
     private fun setUsersTabs() {
-        binding.singleUserContainerViewPager2.adapter = ItemFragmentAdapter(requireActivity())
+        binding.singleUserContainerViewPager2.adapter = UserFragmentAdapter(requireActivity())
         TabLayoutMediator(
             binding.singleUserCardsListTabLayout,
             binding.singleUserContainerViewPager2
@@ -77,5 +83,16 @@ class SingleUserFragment : Fragment() {
 
     companion object {
         const val ARG_USER = "user"
+    }
+
+    private inner class UserFragmentAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = 2
+
+        override fun createFragment(position: Int): Fragment {
+            return CardListFragment.newInstance(
+                position == 0,
+                user.cards as ArrayList<DBMagicCard>
+            )
+        }
     }
 }
