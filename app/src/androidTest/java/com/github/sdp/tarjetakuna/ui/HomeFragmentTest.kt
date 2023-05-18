3b276580c6d3c5d3cc7651210b89f380271aea3a
Cocograal <willi1maril@gmail.com>
2023-05-18 14:28:34 +0200

    emulator added to tests

--- app/src/androidTest/java/com/github/sdp/tarjetakuna/ui/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp/tarjetakuna/ui/HomeFragmentTest.kt
@@ -17,13 +17,11 @@ import com.github.sdp.tarjetakuna.database.DBMagicCard
 import com.github.sdp.tarjetakuna.database.local.LocalDatabaseProvider
 import com.github.sdp.tarjetakuna.ui.authentication.Authenticator
 import com.github.sdp.tarjetakuna.ui.authentication.SignIn
+import com.github.sdp.tarjetakuna.utils.FBEmulator
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 import org.hamcrest.Matchers.equalTo
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 import org.mockito.Mockito
 
@@ -37,7 +35,13 @@ class HomeFragmentTest {
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
-    
+
+    companion object {
+        @get:ClassRule
+        @JvmStatic
+        val fbEmulator = FBEmulator()
+    }
+
     private lateinit var activityRule: ActivityScenario<MainActivity>
 
     @Before
@@ -45,6 +49,7 @@ class HomeFragmentTest {
         // mock the authentication
         val mockedAuth = Mockito.mock(Authenticator::class.java)
         Mockito.`when`(mockedAuth.isUserLoggedIn()).thenReturn(true)
+        Mockito.`when`(mockedAuth.getUserUID()).thenReturn("test")
         SignIn.setSignIn(mockedAuth)
 
         // close the database that could have been opened because of the previous tests
