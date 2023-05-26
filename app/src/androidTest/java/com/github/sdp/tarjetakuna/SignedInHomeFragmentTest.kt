b63aaa27bf85ae3366d4ce9631a4d57cbe8bb3e4
Anne-Marie Rusu <annierusu@gmail.com>
2023-05-26 02:18:01 +0200

    mocked auth

--- app/src/androidTest/java/com/github/sdp/tarjetakuna/SignedInHomeFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp/tarjetakuna/SignedInHomeFragmentTest.kt
@@ -2,19 +2,24 @@ package com.github.sdp.tarjetakuna
 
 import android.Manifest
 import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
+import com.github.sdp.tarjetakuna.database.FirebaseDB
+import com.github.sdp.tarjetakuna.database.local.LocalDatabaseProvider
+import com.github.sdp.tarjetakuna.ui.authentication.Authenticator
+import com.github.sdp.tarjetakuna.ui.authentication.SignIn
 import com.github.sdp.tarjetakuna.utils.FBEmulator
+import com.google.android.gms.tasks.Tasks
+import org.junit.*
 import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.ClassRule
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class SignedInHomeFragmentTest {
@@ -40,6 +45,26 @@ class SignedInHomeFragmentTest {
         activityRule.scenario.onActivity { activity ->
             activity.changeFragment(R.id.nav_home)
         }
+        val mockedAuth = Mockito.mock(Authenticator::class.java)
+        Mockito.`when`(mockedAuth.isUserLoggedIn()).thenReturn(true)
+        Mockito.`when`(mockedAuth.getUserUID()).thenReturn("homefrag_test")
+        Mockito.`when`(mockedAuth.getUserDisplayName()).thenReturn("John Doe")
+        SignIn.setSignIn(mockedAuth)
+
+        LocalDatabaseProvider.setDatabase(
+            ApplicationProvider.getApplicationContext(),
+            LocalDatabaseProvider.CARDS_DATABASE_NAME,
+            true
+        )
+        val task = FirebaseDB().clearDatabase()
+        Tasks.await(task, 5, TimeUnit.SECONDS)
+    }
+
+    @After
+    fun tearDown() {
+        LocalDatabaseProvider.closeDatabase(LocalDatabaseProvider.CARDS_DATABASE_NAME)
+        val task = FirebaseDB().clearDatabase()
+        Tasks.await(task, 5, TimeUnit.SECONDS)
     }
 
     @Test
@@ -48,7 +73,7 @@ class SignedInHomeFragmentTest {
         activityRule.scenario.onActivity { activity ->
             val navController =
                 Navigation.findNavController(activity, R.id.nav_host_fragment_content_drawer)
-            // check if it goes back to nav_authentication first and then to nav_authentication_button
+            // todo check if it goes back to nav_authentication first and then to nav_authentication_button
 //            assertEquals(
 //                navController.previousBackStackEntry?.destination?.id,
 //                R.id.nav_authentication
